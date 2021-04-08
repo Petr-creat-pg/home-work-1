33327e4a1ec4c087d68904ded70850b856ba2e6f
Petr-creat-pg <81649008+Petr-creat-pg@users.noreply.github.com>
2021-04-08 19:14:17 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,6 +1,16 @@
-print('введите время, часы')
-time= int(input())
-sec= time//3600
-minut= (time//60)%60
-chas= time%60
-print(str(chas)+':'+str(minut)+':'+str(sec))
+my_list = ['a', 'b', 'c', 'd', 'e']
+if len(my_list) % 2 == 0:
+    i = 0
+    while i < len(my_list):
+        el = my_list[i]
+        my_list[i] = my_list[i+1]
+        my_list[i+1] = el
+        i += 2
+else:
+    i = 0
+    while i < len(my_list) - 1:
+        el = my_list[i]
+        my_list[i] = my_list[i + 1]
+        my_list[i + 1] = el
+        i += 2
+print(my_list)
--- 4.py
+++ 4.py
@@ -1,8 +1,6 @@
-a = int(input())
-b = a%10
-a = a//10
-while a > 0:
-    if a%10 > b:
-        b = a%10
-    a = a//10
-print(b)
+my_str = input("Enter string: ")
+a = my_str.split(' ')
+for i, el in enumerate(a, 1):
+    if len(el) > 10:
+        el = el[0:10]
+    print(f"{i}. - {el}")
